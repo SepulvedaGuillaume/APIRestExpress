d8ae201cd7a392ed3d4e1aa49388f637372f3d58
Sepulveda <sepulveda.guillaume@gmail.com>
2024-05-29 16:11:49 +0200

    add category table and some endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import adsRoutes from "./ads.routes";
 import moviesRoutes from "./movies.routes";
 import adsSqlRoutes from "./ads-sql.routes";
+import categoriesRoutes from "./categories.routes";
 
 const router = express.Router();
 
 router.use("/ads", adsRoutes);
 router.use("/movies", moviesRoutes);
 router.use("/ads-sql", adsSqlRoutes);
+router.use("/categories", categoriesRoutes);
 
 export default router;
