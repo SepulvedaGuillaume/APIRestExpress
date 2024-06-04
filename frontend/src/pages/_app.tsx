f78b7b901f0109f5609eb44b8589f8c39fea90f2
Sepulveda <sepulveda.guillaume@gmail.com>
2024-06-04 12:22:33 +0200

    fix

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
-import Layout from "@/components/Layout";
+import Layout from "@/pages/Layout";
 
 function App({ Component, pageProps }: AppProps) {
   return (
